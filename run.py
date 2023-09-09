04564a06ac071e43cf49bae70c85285b0346ae0c
renebaumann3000 Baumann <rene.baumann84@outlook.com>
2023-09-09 12:00:28 +0000

    set the self conditions

--- run.py
+++ run.py
@@ -7,3 +7,10 @@ class Character:
     WIN_HUMANITY_THRESHOLD = 100
     LOSS_EMPATHY_THRESHOLD = 0
     LOSS_HUMANITY_THRESHOLD = 0
+
+# Initializes character with name, health, empathy, and humanity
+def __init__(self, name, health=100, empathy=15, humanity=15):
+    self.name = name
+    self.health = health
+    self.empathy = empathy
+    self.humanity = humanity
