0d27885c34ac01d71a90ccc568b245dc6d330e82
renebaumann3000 Baumann <rene.baumann84@outlook.com>
2023-09-12 16:00:00 +0000

    set reset for Reset player and opponent classes

--- run.py
+++ run.py
@@ -205,6 +205,11 @@ class Game:
             if play_again != "y":
                 break
 
+                # Reset player and opponent classes for a new game
+                def reset_all(self):
+        self.player.reset()
+        self.opponent.reset()
+
                 # Function to format and print the grid
 
     def display_grid(self):
